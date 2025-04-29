26742ebe1a31e638c9c4dde0a905ab42ade147a5
Gowri239 <gowrimopuru14@gmail.com>
2025-04-29 18:46:56 +0530

    add: validation on submit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/validation";
 
 const Login = () => {
   const [isSignInForm, setIsSignForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignForm(!isSignInForm);
+  };
+
+  const handleSubmit = () => {
+    const message = checkValidateData(
+      name.current.value,
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(message);
   };
 
   return (
@@ -17,28 +32,40 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className="rounded-lg absolute w-4/12 p-12 bg-black/85 my-30 mx-auto right-0 left-0 text-white">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="rounded-lg absolute w-4/12 p-12 bg-black/85 my-30 mx-auto right-0 left-0 text-white"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             className="border border-gray-500 p-4 my-2 w-full rounded-lg"
             type="text"
             placeholder="Full Name"
           />
         )}
         <input
+          ref={email}
           className="border border-gray-500 p-4 my-2 w-full rounded-lg"
           type="text"
-          placeholder="Email or mobile number"
+          placeholder="Email adress"
         />
         <input
+          ref={password}
           className="p-4 border border-gray-500 my-2 w-full rounded-lg"
           type="password"
           placeholder="Password"
         />
-        <button className="bg-red-700 p-2 my-5 w-full rounded-lg cursor-pointer">
+        <p className="font-bold text-red-700 text-lg px-1 my-2">
+          {errorMessage}
+        </p>
+        <button
+          className="bg-red-700 p-2 my-5 w-full rounded-lg cursor-pointer"
+          onClick={handleSubmit}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <div className="flex">
