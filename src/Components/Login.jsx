bea014de5c66273ef4b1cc82401b8da068b262f6
Gowri239 <gowrimopuru14@gmail.com>
2025-04-29 18:13:52 +0530

    add: sign up form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -11,21 +17,41 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className="rounded-lg absolute w-4/12 p-12 bg-black/80 my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+      <form className="rounded-lg absolute w-4/12 p-12 bg-black/85 my-30 mx-auto right-0 left-0 text-white">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            className="border border-gray-500 p-4 my-2 w-full rounded-lg"
+            type="text"
+            placeholder="Full Name"
+          />
+        )}
         <input
-          className="bg-gray-700 p-4 my-2 w-full rounded-lg"
+          className="border border-gray-500 p-4 my-2 w-full rounded-lg"
           type="text"
           placeholder="Email or mobile number"
         />
         <input
-          className="bg-gray-700 p-4 my-2 w-full rounded-lg"
+          className="p-4 border border-gray-500 my-2 w-full rounded-lg"
           type="password"
           placeholder="Password"
         />
-        <button className="bg-red-700 p-4 my-6 w-full rounded-lg">
-          Sign In
+        <button className="bg-red-700 p-2 my-5 w-full rounded-lg cursor-pointer">
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <div className="flex">
+          <p className="text-gray-300">
+            {isSignInForm ? "New to Netflix?" : "Already a registered user?"}
+          </p>
+          <p
+            className="font-bold pl-1 cursor-pointer"
+            onClick={toggleSignInForm}
+          >
+            {isSignInForm ? "Sign up now" : "Sign in"}
+          </p>
+        </div>
       </form>
     </div>
   );
