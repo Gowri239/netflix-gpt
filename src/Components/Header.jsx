bb1c6f580ec38450364d8de570191476a32e2feb
Gowri239 <gowrimopuru14@gmail.com>
2025-04-30 15:35:58 +0530

    fix: unsubscribe to the onAuthStateChanged when header component unmount

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName } = user;
         dispatch(addUser({ uid, email, displayName }));
@@ -21,6 +21,8 @@ const Header = () => {
         navigate("/");
       }
     });
+
+    return () => unsubscribe();
   }, []);
 
   const handleSignOut = () => {
