2ed3f48aaa40ab4aa4f774cf1499f487a5f22ee5
Gowri239 <gowrimopuru14@gmail.com>
2025-05-01 11:24:12 +0530

    add: language constant files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,3 +16,11 @@ export const BG_IMAGE =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "telugu", name: "Telugu" },
+  { identifier: "kannada", name: "Kannada" },
+  { identifier: "tamil", name: "Tamil" },
+];
---
+++ src/utils/languageConstants.js
@@ -0,0 +1,24 @@
+const lang = {
+  en: {
+    search: "Search",
+    gptSearchPlaceholder: "What would you like to watch today?",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे?",
+  },
+  telugu: {
+    search: "శోధించండి",
+    gptSearchPlaceholder: "ఈరోజు మీరు ఏమి చూడాలనుకుంటున్నారు?",
+  },
+  kannada: {
+    search: "ಹುಡುಕು",
+    gptSearchPlaceholder: "ನೀವು ಇಂದು ಏನು ನೋಡಲು ಬಯಸುತ್ತೀರಿ?",
+  },
+  tamil: {
+    search: "தேடு",
+    gptSearchPlaceholder: "இன்று நீங்கள் என்ன பார்க்க விரும்புகிறீர்கள்?",
+  },
+};
+
+export default lang;
